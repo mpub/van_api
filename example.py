ec76e3040ce5d4b344498b384c76b91826f56c4d
Brian Sutherland <brian@vanguardistas.net>
2012-05-25 16:04:58 +0200

    use logging in example

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 import van_api
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.Info, format='%(levelname)s:%(message)s')
 
 # Connect to the API
 # The example API key here allows using the http://demo.metropublisher.com/ api as a public user
@@ -13,11 +13,11 @@ credentials = van_api.ClientCredentialsGrant('mxvsm129bm7RgcGRYedzLersZXGQSwQjMi
 api = van_api.API('api.metropublisher.com', credentials)
 
 # Get a list of the sections and print them
-pprint("Getting Available Resources")
+logging.info("Getting Available Resources")
 result = api.GET('/1')
 pprint(result)
 
 # Get a list of the sections and print them
-pprint("Getting Sections")
+logging.info("Getting Sections")
 result = api.GET('/1/sections?fields=title-urlname-url')
 pprint(result)
