cf72457f5d9e9a0f6324ad6951f7d31232ea8b94
Brian Sutherland <brian@vanguardistas.net>
2013-01-24 14:03:48 +0100

    factor out a connection manager object
    give the credentials their own connection manager
    
    TODO: factor out a separate handler object

--- tests.py
+++ tests.py
@@ -13,20 +13,20 @@ class TestCredentials(TestCase):
         self.assertRaises(NotImplementedError, one.access_token, mock_API())
 
     def test_token(self):
-        from van_api import Credentials
+        from van_api import Credentials, _HTTPConnection
+        from mock import Mock
         one = Credentials()
+        one.conn = Mock(set_spec=_HTTPConnection)
         api = mock_API()
         result = one._token(api, dict(param='param value'), )
-        api.retry.assert_called_once_with(
-                api.http,
+        one.conn.http_retry.assert_called_once_with(
                 'POST',
                 '/oauth/token',
-                host='go.vanguardistas.net',
                 body='param=param+value',
                 headers={'Content-Type': 'application/x-www-form-urlencoded'},
                 handler=api.handle
                 )
-        self.assertEqual(result, api.retry())
+        self.assertEqual(result, one.conn.http_retry())
 
 
 class TestClientCredentialsGrant(TestCase):
@@ -72,143 +72,12 @@ class TestAPI(TestCase):
         import logging
         from van_api import API
         one = API('host', 'creds')
-        self.assertEqual(one.host, 'host')
+        self.assertEqual(one.conn.host, 'host')
         self.assertEqual(one._creds, 'creds')
-        self.assertEqual(one._conn, {})
+        self.assertEqual(one.conn._conn, None)
         self.assertEqual(one.logger, logging)
-        self.assertEqual(one._conn_factory, HTTPSConnection)
-
-    def test_retry_no_exception(self):
-        func = mock.Mock()
-        api = self._one()
-        result = api.retry(func, 55, a=42)
-        func.assert_called_once_with(55, a=42)
-        self.assertEqual(func(), result)
-
-    def test_retry_logging(self):
-        from van_api import Retryable
-        import logging
-        from mock import call
-        func = mock.Mock()
-        logger = mock.Mock(spec_set=logging.Logger)
-        class MyException(Retryable):
-            pass
-        func.side_effect = MyException('oops')
-        api = self._one(logger=logger)
-        result = self.assertRaises(Exception, api.retry, func, 55, a=42)
-        expected = [call('Attempt %s failed', n + 1, exc_info=True) for n
-                in range(5)]
-        self.assertEqual(
-                logger.warn.call_args_list,
-                expected)
-
-    def test_retry_non_retryable_exception(self):
-        func = mock.Mock()
-        class MyException(Exception):
-            pass
-        func.side_effect = MyException('oops')
-        api = self._one()
-        self.assertRaises(MyException, api.retry, func, 55, a=42)
-        func.assert_called_once_with(55, a=42)
-
-    def test_retry_retryable_exception_exhausted(self):
-        from van_api import Retryable
-        from mock import call
-        func = mock.Mock()
-        class MyException(Retryable):
-            pass
-        func.side_effect = MyException('oops')
-        api = self._one()
-        self.assertRaises(MyException, api.retry, func, 55, a=42)
-        self.assertEqual(func.call_args_list, [call(55, a=42)] * 5)
-
-    def test_retry_retryable_wrapping_other_exception(self):
-        import sys
-        from van_api import Retryable
-        from mock import call
-        func = mock.Mock()
-        class MyException(Exception):
-            pass
-        def side_effect(one, a=0):
-            try:
-                raise MyException()
-            except:
-                raise Retryable("A retryable error", exc_info=sys.exc_info())
-        func.side_effect = side_effect
-        api = self._one()
-        self.assertRaises(MyException, api.retry, func, 55, a=42)
-        self.assertEqual(func.call_args_list, [call(55, a=42)] * 5)
-
-    def test_retry_retryable_exception_third_time_lucky(self):
-        from van_api import Retryable
-        from mock import call
-        class MyException(Retryable):
-            pass
-        return_val = [MyException('oops'), MyException('oops')]
-        def side_effect(b, a=0):
-            if return_val:
-                raise return_val.pop()
-            return a + b
-        func = mock.Mock()
-        func.side_effect = side_effect
-        api = self._one()
-        self.assertEqual(42, api.retry(func, 2, a=40))
-        self.assertEqual(func.call_args_list, [call(2, a=40)] * 3)
-
-    def test_retry_bad_retryable_exception(self):
-        from van_api import Retryable
-        from mock import call
-        func = mock.Mock()
-        class MyException(Retryable):
-            def reraise(self):
-                pass # should raise
-        func.side_effect = MyException('oops')
-        api = self._one()
-        # no infinite loops here
-        self.assertRaises(AssertionError, api.retry, func, 55, a=42)
-
-    def test_http_connect(self):
-        one = self._one(host='myapi')
-        one.handle = mock.Mock()
-        self.assertEqual(one._conn, {})
-        one.http('GET', '/')
-        self.assertEqual(one._conn_factory.call_count, 1)
-        self.assertEqual(one._conn['myapi'], one._conn_factory())
-
-    def test_http_retryable_error_raised(self):
-        from van_api import Retryable
-        class Error(Exception):
-            pass
-        one = self._one()
-        one._conn_factory().request.side_effect = Error('boom')
-        try:
-            one.http('GET', '/')
-        except Retryable:
-            import sys
-            e = sys.exc_info()[1]
-            self.assertTrue(isinstance(e.exc_info[1], Error))
-            self.assertEqual(e.exc_info[0], Error)
-        else:
-            self.fail('Exceptiong not raised')
-
-    def test_http_disconnect_on_error(self):
-        from mock import Mock
-        one = self._one()
-        conn = Mock()
-        conn.request.side_effect = Exception('boom')
-        one._conn['myhost'] = conn
-        self.assertRaises(Exception, one.http, 'GET', '/', host='myhost')
-        self.assertEqual(one._conn, {})
-        conn.close.assert_called_once_with()
-
-    def test_http_log_on_error(self):
-        import logging
-        logger = mock.Mock(spec_set=logging.Logger)
-        one = self._one(logger=logger)
-        one._conn_factory().request.side_effect = Exception('boom')
-        one._conn_factory.reset_mock()
-        self.assertRaises(Exception, one.http, 'GET', '/')
-        logger.info.assert_called_once_with('HTTP Connection Error', exc_info=True)
+        self.assertEqual(one.conn.logger, logging)
+        self.assertEqual(one.conn._conn_factory, HTTPSConnection)
 
     def test_get(self):
         one = self._one()
@@ -244,19 +113,6 @@ class TestAPI(TestCase):
         result = one.POST('/', 'data')
         one.request.assert_called_once_with('POST', '/', 'data')
         self.assertEqual(result, one.request())
-
-    def test_http_calls_handle(self):
-        one = self._one(response=dict(
-            headers=[('Header1', 'value'), ],
-            reason='OK',
-            body='',
-            status=200))
-        handle = mock.Mock()
-        result = one.http('POST', '/', handler=handle)
-        handle.assert_called_once_with(
-                {'body': None, 'headers': None, 'host': None, 'url': '/', 'method': 'POST'},
-                {'body': '', 'headers': [('Header1', 'value'), ], 'reason': 'OK', 'status': 200})
-        self.assertEqual(result, handle())
 
     def test_handle_ok(self):
         one = self._one()
@@ -342,16 +198,15 @@ class TestAPI(TestCase):
         self.assertEqual(one._access_token, 'cached')
 
     @mock.patch('van_api.API._get_access_token')
-    @mock.patch('van_api.API.retry')
-    def test_request_ok_data(self, retry, _get_access_token):
+    def test_request_ok_data(self, _get_access_token):
         _get_access_token.return_value = {
                 'token_type': 'bearer',
                 'access_token': 'value'}
         one = self._one()
+        one.conn.http_retry = retry = mock.Mock()
         result = one.request('PUT', '/', 123)
         _get_access_token.assert_called_once_with()
         retry.assert_called_once_with(
-                one.http,
                 'PUT',
                 '/',
                 body='123',
@@ -362,16 +217,15 @@ class TestAPI(TestCase):
         self.assertEqual(result, retry())
 
     @mock.patch('van_api.API._get_access_token')
-    @mock.patch('van_api.API.retry')
-    def test_request_ok_no_data(self, retry, _get_access_token):
+    def test_request_ok_no_data(self, _get_access_token):
         _get_access_token.return_value = {
                 'token_type': 'bearer',
                 'access_token': 'value'}
         one = self._one()
+        one.conn.http_retry = retry = mock.Mock()
         result = one.request('GET', '/')
         _get_access_token.assert_called_once_with()
         retry.assert_called_once_with(
-                one.http,
                 'GET',
                 '/',
                 body=None,
@@ -386,3 +240,179 @@ class TestAPI(TestCase):
         self.assertEqual(
                 one._deserialize(body, 'application/json'),
                 {'abc': 1})
+
+class Test_HTTPConnection(TestCase):
+
+    def _conn_factory(self):
+        try:
+            from http.client import HTTPSConnection
+        except ImportError:
+            #python 2
+            from httplib import HTTPSConnection
+        return mock.Mock(spec_set=HTTPSConnection)
+
+    def _one(self, host='example.org', conn_factory=None, logger=None):
+        from van_api import _HTTPConnection
+        if conn_factory is None:
+            conn_factory = self._conn_factory()
+        return _HTTPConnection(host, conn_factory=conn_factory, logger=logger)
+
+    def test_http_connect(self):
+        one = self._one('example.com')
+        self.assertEqual(one._conn, None)
+        one.http('GET', '/')
+        one._conn_factory.assert_called_once_with('example.com')
+        self.assertEqual(one._conn, one._conn_factory())
+
+    def test_retry_retryable_wrapping_other_exception(self):
+        import sys
+        from van_api import Retryable
+        from mock import call
+        func = mock.Mock()
+        class MyException(Exception):
+            pass
+        def side_effect(one, a=0):
+            try:
+                raise MyException()
+            except:
+                raise Retryable("A retryable error", exc_info=sys.exc_info())
+        func.side_effect = side_effect
+        conn = self._one()
+        conn.http = func
+        self.assertRaises(MyException, conn.http_retry, 55, a=42)
+        self.assertEqual(func.call_args_list, [call(55, a=42)] * 5)
+
+    def test_retry_retryable_exception_third_time_lucky(self):
+        from van_api import Retryable
+        from mock import call
+        class MyException(Retryable):
+            pass
+        return_val = [MyException('oops'), MyException('oops')]
+        def side_effect(b, a=0):
+            if return_val:
+                raise return_val.pop()
+            return a + b
+        func = mock.Mock()
+        func.side_effect = side_effect
+        conn = self._one()
+        conn.http = func
+        self.assertEqual(42, conn.http_retry(2, a=40))
+        self.assertEqual(func.call_args_list, [call(2, a=40)] * 3)
+
+    def test_retry_bad_retryable_exception(self):
+        from van_api import Retryable
+        from mock import call
+        func = mock.Mock()
+        class MyException(Retryable):
+            def reraise(self):
+                pass # should raise
+        func.side_effect = MyException('oops')
+        conn = self._one()
+        conn.http = func
+        # no infinite loops here
+        self.assertRaises(AssertionError, conn.http_retry, 55, a=42)
+
+    def test_retry_retryable_exception_exhausted(self):
+        from van_api import Retryable
+        from mock import call
+        func = mock.Mock()
+        class MyException(Retryable):
+            pass
+        func.side_effect = MyException('oops')
+        conn = self._one()
+        conn.http = func
+        self.assertRaises(MyException, conn.http_retry, 55, a=42)
+        self.assertEqual(func.call_args_list, [call(55, a=42)] * 5)
+
+    def test_http_retryable_error_raised(self):
+        from van_api import Retryable
+        class Error(Exception):
+            pass
+        one = self._one()
+        one._conn_factory().request.side_effect = Error('boom')
+        try:
+            one.http('GET', '/')
+        except Retryable:
+            import sys
+            e = sys.exc_info()[1]
+            self.assertTrue(isinstance(e.exc_info[1], Error))
+            self.assertEqual(e.exc_info[0], Error)
+        else:
+            self.fail('Exceptiong not raised')
+
+    def test_http_disconnect_on_error(self):
+        from mock import Mock
+        one = self._one()
+        conn = Mock()
+        class Boom(Exception):
+            pass
+        conn.request.side_effect = Boom
+        one._conn = conn
+        # A connection error, so retryable
+        from van_api import Retryable
+        self.assertRaises(Retryable, one.http, 'GET', '/')
+        self.assertEqual(one._conn, None)
+        conn.close.assert_called_once_with()
+
+    def test_http_log_on_error(self):
+        import logging
+        logger = mock.Mock(spec_set=logging.Logger)
+        one = self._one()
+        one.logger = logger
+        one._conn_factory().request.side_effect = Exception('boom')
+        one._conn_factory.reset_mock()
+        self.assertRaises(Exception, one.http, 'GET', '/')
+        logger.info.assert_called_once_with('HTTP Connection Error', exc_info=True)
+
+    def test_retry_no_exception(self):
+        func = mock.Mock()
+        conn = self._one()
+        conn.http = func
+        result = conn.http_retry(55, a=42)
+        func.assert_called_once_with(55, a=42)
+        self.assertEqual(func(), result)
+
+    def test_retry_logging(self):
+        from van_api import Retryable
+        import logging
+        from mock import call
+        func = mock.Mock()
+        logger = mock.Mock(spec_set=logging.Logger)
+        class MyException(Retryable):
+            pass
+        func.side_effect = MyException('oops')
+        conn = self._one(logger=logger)
+        conn.http = func
+        result = self.assertRaises(Exception, conn.http_retry, 55, a=42)
+        expected = [call('Attempt %s failed', n + 1, exc_info=True) for n
+                in range(5)]
+        self.assertEqual(
+                logger.warn.call_args_list,
+                expected)
+
+    def test_retry_non_retryable_exception(self):
+        func = mock.Mock()
+        class MyException(Exception):
+            pass
+        func.side_effect = MyException('oops')
+        conn = self._one()
+        conn.http = func
+        self.assertRaises(MyException, conn.http_retry, 55, a=42)
+        func.assert_called_once_with(55, a=42)
+
+    def test_http_calls_handle(self):
+        conn_factory = self._conn_factory()
+        resp = conn_factory().getresponse()
+        resp.getheaders.return_value = [('Header1', 'value'), ]
+        resp.read.return_value = ''
+        resp.status = 200
+        resp.reason = 'OK'
+        conn_factory.reset_mock()
+        one = self._one('www.example.com', conn_factory=conn_factory)
+        handle = mock.Mock()
+        result = one.http('POST', '/', handler=handle)
+        handle.assert_called_once_with(
+                {'body': None, 'headers': None, 'host': 'www.example.com', 'url': '/', 'method': 'POST'},
+                {'body': '', 'headers': [('Header1', 'value'), ], 'reason': 'OK', 'status': 200})
+        self.assertEqual(result, handle())
+
--- van_api.py
+++ van_api.py
@@ -78,11 +78,83 @@ class Retryable(Exception):
             _reraise(self.exc_info)
         raise
 
+class _HTTPConnection(object):
+    """Mixing class deailing with a single HTTP/HTTPS connection.
+
+    Handles connect/disconnect, requests and retries for the connection.
+    """
+
+    _conn = None
+    _conn_factory = None
+
+    def __init__(self, host, conn_factory=httplib.HTTPSConnection, logger=logging):
+        self.host = host
+        self.logger = logger
+        self._conn_factory = conn_factory
+
+    def http(self, method, url, body=None, headers=None, handler=None):
+        """Send a single HTTP request to the API.
+
+        This is a low level method. It fails on all errors.
+        """
+        conn = self._get_conn()
+        request = dict(method=method, host=self.host, url=url, body=body, headers=headers)
+        if self.logger is not None:
+            self.logger.debug('REQUEST:\n%s', pformat(request))
+        try:
+            conn.request(method, url, body=body, headers=headers)
+            resp = conn.getresponse()
+            response = dict(
+                    status = resp.status,
+                    headers = resp.getheaders(),
+                    body = resp.read(),
+                    reason = resp.reason)
+        except:
+            self._disconnect()
+            if self.logger is not None:
+                self.logger.info("HTTP Connection Error", exc_info=True)
+            raise Retryable('HTTP Connection Error', exc_info=sys.exc_info())
+        if self.logger is not None:
+            self.logger.debug('RESPONSE:\n%s', pformat(response))
+        if handler is not None:
+            response = handler(request, response)
+        return response
+
+    def _get_conn(self):
+        if self._conn is not None:
+            return self._conn
+        self._conn = self._conn_factory(self.host)
+        return self._get_conn()
+
+    def http_retry(self, *args, **kw):
+        """Run an http query, retrying on retriable errors"""
+        attempt = 1
+        while True:
+            try:
+                return self.http(*args, **kw)
+            except Retryable:
+                if self.logger is not None:
+                    self.logger.warn('Attempt %s failed',
+                            attempt,
+                            exc_info=True)
+                if attempt >= 5:
+                    exc = sys.exc_info()[1]
+                    exc.reraise()
+                    raise AssertionError("Bad retryable exception: %s" % exc)
+            attempt += 1
+
+    def _disconnect(self):
+        conn = self._conn
+        self._conn = None
+        if conn is not None:
+            conn.close()
+
 
 class Credentials(object):
     """Abstract class representing credentials to access the API"""
 
-    token_host = 'go.vanguardistas.net'
+    def __init__(self, host='go.vanguardistas.net', **kw):
+        self.conn = _HTTPConnection(host, **kw)
 
     def access_token(self, api):
         """This method is called to get the access token.
@@ -94,8 +166,7 @@ class Credentials(object):
     def _token(self, api, data):
         data = urlencode(data)
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        return api.retry(api.http, 'POST', '/oauth/token',
-                host=self.token_host,
+        return self.conn.http_retry('POST', '/oauth/token',
                 body=data,
                 headers=headers,
                 handler=api.handle)
@@ -103,7 +174,8 @@ class Credentials(object):
 
 class ClientCredentialsGrant(Credentials):
 
-    def __init__(self, api_key, api_secret):
+    def __init__(self, api_key, api_secret, **kw):
+        Credentials.__init__(self, **kw)
         self.api_key = api_key
         self.api_secret = api_secret
 
@@ -114,17 +186,15 @@ class ClientCredentialsGrant(Credentials):
         return self._token(api, data)
 
 
-class API(object):
+class API(_HTTPConnection):
     """A proxy object for the MP api."""
 
     _access_token = None
 
-    def __init__(self, host, credentials=None, logger=logging, conn_factory=httplib.HTTPSConnection):
-        self._conn = {}
-        self.host = host
+    def __init__(self, host, credentials=None, logger=logging, **kw):
+        self.conn = _HTTPConnection(host, logger=logger, **kw)
         self.logger = logger
         self._creds = credentials
-        self._conn_factory = conn_factory
 
     def GET(self, url):
         """GET a resource"""
@@ -162,35 +232,7 @@ class API(object):
             headers['Authorization'] = self._auth_header(access_token)
         data, data_headers = self._serialize(data)
         headers.update(data_headers)
-        return self.retry(self.http, method, url, body=data, headers=headers, handler=self.handle)
-
-    def http(self, method, url, host=None, body=None, headers=None, handler=None):
-        """Send a single HTTP request to the API.
-
-        This is a low level method. It fails on all errors.
-        """
-        conn = self._get_conn(host)
-        request = dict(method=method, host=host, url=url, body=body, headers=headers)
-        if self.logger is not None:
-            self.logger.debug('REQUEST:\n%s', pformat(request))
-        try:
-            conn.request(method, url, body=body, headers=headers)
-            resp = conn.getresponse()
-            response = dict(
-                    status = resp.status,
-                    headers = resp.getheaders(),
-                    body = resp.read(),
-                    reason = resp.reason)
-        except:
-            self._disconnect(host)
-            if self.logger is not None:
-                self.logger.info("HTTP Connection Error", exc_info=True)
-            raise Retryable('HTTP Connection Error', exc_info=sys.exc_info())
-        if self.logger is not None:
-            self.logger.debug('RESPONSE:\n%s', pformat(response))
-        if handler is not None:
-            response = handler(request, response)
-        return response
+        return self.conn.http_retry(method, url, body=data, headers=headers, handler=self.handle)
 
     def handle(self, request, response):
         handler = getattr(self, '_handle_status_%s' % response['status'], self._handle_error)
@@ -225,31 +267,6 @@ class API(object):
 
     _handle_status_201 = _handle_status_200
 
-    def _get_conn(self, host=None):
-        if host is None:
-            host = self.host
-        conn = self._conn.get(host, None)
-        if conn is not None:
-            return conn
-        self._conn[host] = self._conn_factory(host)
-        return self._get_conn(host)
-
-    def retry(self, func, *args, **kw):
-        attempt = 1
-        while True:
-            try:
-                return func(*args, **kw)
-            except Retryable:
-                if self.logger is not None:
-                    self.logger.warn('Attempt %s failed',
-                            attempt,
-                            exc_info=True)
-                if attempt >= 5:
-                    exc = sys.exc_info()[1]
-                    exc.reraise()
-                    raise AssertionError("Bad retryable exception: %s" % exc)
-            attempt += 1
-
     def _get_access_token(self):
         if self._access_token is not None:
             return self._access_token
@@ -261,11 +278,6 @@ class API(object):
     def _auth_header(self, token):
         assert token['token_type'] == 'bearer'
         return 'bearer %s' % token['access_token']
-
-    def _disconnect(self, host):
-        conn = self._conn.pop(host, None)
-        if conn is not None:
-            conn.close()
 
     def _serialize(self, data):
         if data is None:
