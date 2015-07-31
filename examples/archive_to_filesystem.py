b23ee3f50af1852356ba4f6c5c7efb43d99ec894
Kristoph Kirchner <kristoph@vanguardistas.net>
2015-07-31 10:38:51 +0200

    added downloading of media files to archive_to_filesystem.py, fixes mpub/issue#3762

--- examples/archive_to_filesystem.py
+++ examples/archive_to_filesystem.py
@@ -38,8 +38,12 @@ class Spider:
     def run(self):
         todo_urls = set([self.start_url])
         seen_urls = set([])
+        media_todo_urls = set([])
+        media_seen_urls = set([])
         while todo_urls:
-            self.spider_one(seen_urls, todo_urls)
+            self.spider_one(seen_urls, todo_urls, media_todo_urls)
+        while media_todo_urls:
+            self.spider_media(media_seen_urls, media_todo_urls)
 
     def get_outfile(self, url):
         url = urlparse(url)
@@ -54,13 +58,18 @@ class Spider:
             if h.lower() == 'content-type':
                 content_type = v.split(';')[0].strip()
                 break
-        outfile = self.get_outfile(request['url'])
         if content_type == 'application/json':
+            outfile = self.get_outfile(request['url'])
             body = resp.read()
             outfile = '%s.json' % outfile
             with open(outfile, 'wb') as f:
                 f.write(body)
         else:
+            # expects a file download url like: /{iid}/files/{uuid}/download/...
+            download_url = request['url']
+            download_url = download_url.split('/download')[0]
+            outfile = self.get_outfile(download_url)
+            outfile = '%s.data' % outfile
             body = None
             with open(outfile, 'wb') as f:
                 van_api.write_body_to_file(resp, f)
@@ -70,7 +79,27 @@ class Spider:
                 body=body,
                 reason=resp.reason)
 
-    def spider_one(self, seen_urls, todo_urls):
+    def http_handler_media(self, request, resp):
+        headers = resp.getheaders()
+        for h, v in headers:
+            if h.lower() == 'content-type':
+                content_type = v.split(';')[0].strip()
+                break
+        if content_type == 'application/json':
+            body = resp.read()
+            body_json = json.loads(body)
+            if body_json.get('items'):
+                outfile = self.get_outfile(request['url'])
+                outfile = '%s.json' % outfile
+                with open(outfile, 'wb') as f:
+                    f.write(json.dumps(body_json['items']))
+        return dict(
+                status=resp.status,
+                headers=headers,
+                body=body,
+                reason=resp.reason)
+
+    def spider_one(self, seen_urls, todo_urls, media_todo_urls):
         url = todo_urls.pop()
         logging.info('Spidering: %s' % url)
         assert url not in seen_urls
@@ -87,8 +116,20 @@ class Spider:
                         url = item[0]
                     else:
                         url = item['url']
+                    if '%s/content/' % self.start_url in url:
+                        media_todo_urls.add('%s/media' % url)
                     go_urls.add(url)
         todo_urls.update(go_urls - seen_urls)
+
+    def spider_media(self, seen_urls, todo_urls):
+        url = todo_urls.pop()
+        logging.info('Spidering media: %s' % url)
+        assert url not in seen_urls
+        seen_urls.add(url)
+        go_urls = set([])
+        resp = self.api.request('GET', url, http_handler=self.http_handler_media)
+        todo_urls.update(go_urls - seen_urls)
+
 
 def connect(api_key, api_secret, endpoint='api.metropublisher.com'):
     logging.info("Connection to the API")
